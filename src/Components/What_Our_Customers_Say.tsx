b4ce31d56a6f4a70398416997b7e07df967b6bb5
elmira1385 <elmirashokriyan64@gmail.com>
2026-02-06 14:38:08 +0330

    done

--- src/Components/What_Our_Customers_Say.tsx
+++ src/Components/What_Our_Customers_Say.tsx
@@ -1,7 +1,8 @@
 
 import Comment from "./Comment";
 import { motion } from "motion/react";
-
+import p1 from "/src/images/women1.png"
+import p2 from "/src/images/women2.png"
 const What_Our_Customers_Say = () => {
   return (
     <motion.div initial={
@@ -23,13 +24,13 @@ const What_Our_Customers_Say = () => {
         </p>
       </div>
       <div className="flex flex-col gap-4 sm:flex-row sm:px-42 sm:gap-8">
-       <Comment image="./src/images/women1.png" firstName="Emma Rodriguez" address="Barcelona, Spain">
+       <Comment image={p1} firstName="Emma Rodriguez" address="Barcelona, Spain">
        "I've rented cars from various companies, but the experience with CarRental was exceptional."
        </Comment>
-       <Comment image="./src/images/women2.png" firstName="John Smith" address="New York, USA">
+       <Comment image={p2} firstName="John Smith" address="New York, USA">
        "CarRental made my trip so much easier. The car was delivered right to my door, and the customer service was fantastic!"
        </Comment>
-       <Comment image="./src/images/women1.png" firstName="Ava Johnson" address="Sydney, Australia">
+       <Comment image={p1} firstName="Ava Johnson" address="Sydney, Australia">
        "I highly recommend CarRental! Their fleet is amazing, and I always feel like I'm getting the best deal with excellent service."
        </Comment>
       </div>
